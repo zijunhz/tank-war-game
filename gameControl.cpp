228727d6445754e7c17d10e782aa04605e439157
Zijun Yang <zijunhz@126.com>
2022-08-06 21:05:50 +0800

    add license

--- gameControl.cpp
+++ gameControl.cpp
@@ -1,3 +1,20 @@
+// Copyright (C) 2022 Zijun Yang <zijun.yang@outlook.com>
+//
+// This file is part of Tank War Game with CLI.
+//
+// Tank War Game with CLI is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tank War Game with CLI is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Tank War Game with CLI.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "gameControl.h"
 #include <cctype>
 #include <cmath>
